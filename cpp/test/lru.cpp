af4afd0d83432f68c84161c95cc1084f627678f9
yuling <asdf168gh@gmail.com>
2016-02-12 04:02:31 +0800

    remove value_list

--- cpp/test/lru.cpp
+++ cpp/test/lru.cpp
@@ -1,10 +1,9 @@
-// #include <iostream>
+#include <iostream>
 #include <cstring>
 #include <map>
 class LRUCache{
 public:
     int *key_list;
-    int *value_list;
     int max_capacity;
     int now_capacity;
 	std::map<int,int> mymap;
@@ -13,7 +12,6 @@ public:
         max_capacity = capacity;
         now_capacity = 0;
         key_list = new int[capacity];
-        value_list = new int[capacity];
         for(int i = 0; i < capacity; i++){
             key_list[i] = -1;
         }
@@ -21,29 +19,25 @@ public:
 
     int get(int key) {
         int result = -1;
-		if(mymap.count(key)>0){
-			for(int i = 0; i < now_capacity; i++){ // find
-				if(key_list[i] == key){
-					result = value_list[i];
-					memcpy(key_list+1, key_list, sizeof(int)*i);
-					memcpy(value_list+1, value_list, sizeof(int)*i);
-					value_list[0] = result;
+		if(mymap.count(key)>0){ // check exist
+			result = mymap[key];
+			for(int i = 0; i < now_capacity; i++){ // find key location
+				if(key_list[i] == key){ // found
+					memcpy(key_list+1, key_list, sizeof(int)*i); // move key
 					key_list[0] = key;
 					break;
 				}
 			}
 		}
-        // std::cout << key << ' ' << result << std::endl;
+        std::cout << key << ' ' << result << std::endl;
         return result;
     }
 
     void set(int key, int value) {
-		if(mymap.count(key)>0){
-			for(int i = 0; i < now_capacity; i++){ // find key
-				if(key_list[i] == key){
-					memcpy(key_list+1, key_list, sizeof(int)*i);
-					memcpy(value_list+1, value_list, sizeof(int)*i);
-					value_list[0] = value;
+		if(mymap.count(key)>0){ // check exist
+			for(int i = 0; i < now_capacity; i++){ // find key location
+				if(key_list[i] == key){ // found
+					memcpy(key_list+1, key_list, sizeof(int)*i); // move key
 					key_list[0] = key;
 					break;
 				}
@@ -57,9 +51,7 @@ public:
                 mymap.erase(key_list[max_capacity-1]);
             }
             memcpy(key_list+1, key_list, sizeof(int)*(now_capacity-1));
-            memcpy(value_list+1, value_list, sizeof(int)*(now_capacity-1));
             key_list[0] = key;
-            value_list[0] = value;
 			mymap[key] = value;
 		}
     }
