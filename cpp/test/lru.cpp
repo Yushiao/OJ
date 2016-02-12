09e62960e0b71b33cd275ea8091cead3d22219cd
yuling <asdf168gh@gmail.com>
2016-02-12 17:04:33 +0800

    stl unordered_map+list

--- cpp/test/lru.cpp
+++ cpp/test/lru.cpp
@@ -1,56 +1,44 @@
-// #include <iostream>
+#include <iostream>
 #include <cstring>
-#include <map>
-#include <vector>
+#include <unordered_map>
+#include <list>
+
 class LRUCache{
 public:
-    // int *key_list;
-    int max_capacity;
-    int now_capacity;
-	std::map<int,int> mymap;
-	std::vector<int> mykey;
+    int m_capacity;
+    typedef std::list<std::pair<int, int>>::iterator list_iterator;
+    typedef std::unordered_map<int, list_iterator>::iterator map_iterator;
+    std::unordered_map<int, list_iterator> m_map; // first: key, second: list iterator
+    std::list<std::pair<int, int>> m_list; // first: key, second: value
 
     LRUCache(int capacity) {
-        max_capacity = capacity;
-		now_capacity = 0;
+        m_capacity = capacity;
     }
 
     int get(int key) {
-        int result = -1;
-		if(mymap.count(key)>0){ // check exist
-			result = mymap[key];
-			for(int i = 0; i < now_capacity; i++){ // find key location
-				if(mykey[i] == key){ // found
-					mykey.erase(mykey.begin() + i);
-					mykey.push_back(key);
-					break;
-				}
-			}
-		}
-        // std::cout << key << ' ' << result << std::endl;
-        return result;
+        map_iterator found = m_map.find(key);
+        if(found == m_map.end()){ // key does not exist
+            return -1;
+        }
+        m_list.splice(m_list.begin(), m_list, found->second); // move to begin
+        return found->second->second;
     }
 
     void set(int key, int value) {
-		if(mymap.count(key)>0){ // check exist
-			for(int i = 0; i < now_capacity; i++){ // find key location
-				if(mykey[i] == key){ // found
-					mykey.erase(mykey.begin() + i);
-					mykey.push_back(key);
-					break;
-				}
-			}
-			mymap[key] = value;
-		}
-		else{
-			mykey.push_back(key);
-			mymap[key] = value;
-			now_capacity++;
-            if(now_capacity > max_capacity) {
-                mymap.erase(mykey[0]);
-				mykey.erase(mykey.begin());
-				now_capacity--;
-            }
-		}
+        map_iterator found = m_map.find(key);
+        if(found != m_map.end()){ // key exists
+            m_list.splice(m_list.begin(), m_list, found->second); // move to begin
+            found->second->second = value;
+            return;
+        }
+
+        if(m_map.size() == m_capacity){
+           int del_key = m_list.back().first;
+           m_list.pop_back(); //remove node in list;
+           m_map.erase(del_key); //remove key in map
+        }
+
+        m_list.emplace_front(key, value); // create node at begin
+        m_map[key] = m_list.begin(); // create node in map
     }
 };
